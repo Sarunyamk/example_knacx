f2284956757f3c5f39c4ad991ba25fb20f575f60
Sarunyamk <Sarunya46mk@gmail.com>
2025-01-09 04:46:38 +0700

    edit -route

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -1,4 +1,53 @@
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+// import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+// import HomePage from './../layouts/HomePage';
+// import Register from './../components/users/Register';
+// import Login from './../components/users/Login';
+// import Layout from '../layouts/Layout';
+// import Pagination from '../components/Pagination/Pagination';
+// import Shop from '../components/admin/shop';
+
+// import ProtectedRoute from '../routes/ProtectedRoute';
+// import NotFound from '../layouts/NotFound';
+
+
+
+
+// const pageRouter = createBrowserRouter([
+
+//     {
+//         path: "/",
+//         element: <Layout />,
+//         children: [
+//             { index: true, element: <HomePage /> },
+//             { path: "register", element: <Register /> },
+//             { path: "login", element: <Login /> },
+//             { path: "shop", element: <Pagination /> },
+//             {
+//                 path: "admin-create",
+//                 element: (
+//                     <ProtectedRoute allowedRoles={["admin"]}>
+//                         <Shop />
+//                     </ProtectedRoute>
+//                 ),
+//             },
+
+//         ],
+//     }, {
+//         path: "*",
+//         element: <NotFound />
+//     }
+
+// ])
+
+// export default function AppRouter() {
+//     return (
+//         <div>
+//             <RouterProvider router={pageRouter} />
+//         </div>
+//     )
+// }
+
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './../layouts/HomePage';
 import Register from './../components/users/Register';
 import Login from './../components/users/Login';
@@ -9,40 +58,19 @@ import Shop from '../components/admin/shop';
 import ProtectedRoute from '../routes/ProtectedRoute';
 import NotFound from '../layouts/NotFound';
 
+const AppRouter = () => (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
+                <Route path="register" element={<Register />} />
+                <Route path="login" element={<Pagination />} />
+                <Route path="shop" element={<Login />} />
+                <Route path="admin-create" element={<ProtectedRoute allowedRoles={["admin"]}><Shop /></ProtectedRoute>} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    </Router>
+);
 
-
-
-const pageRouter = createBrowserRouter([
-
-    {
-        path: "/",
-        element: <Layout />,
-        children: [
-            { index: true, element: <HomePage /> },
-            { path: "register", element: <Register /> },
-            { path: "login", element: <Login /> },
-            { path: "shop", element: <Pagination /> },
-            {
-                path: "admin-create",
-                element: (
-                    <ProtectedRoute allowedRoles={["admin"]}>
-                        <Shop />
-                    </ProtectedRoute>
-                ),
-            },
-
-        ],
-    }, {
-        path: "*",
-        element: <NotFound />
-    }
-
-])
-
-export default function AppRouter() {
-    return (
-        <div>
-            <RouterProvider router={pageRouter} />
-        </div>
-    )
-}
+export default AppRouter;
